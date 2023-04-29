19e8d8dcba80e3d52d88b7d298a084acbde84178
elliotthuryk <elliott.huryk@gmail.com>
2023-04-29 16:45:58 -0400

    Added exit flag and pydirectinput to use in games

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import cv2, math, pyautogui, time
+import cv2, math, pyautogui, pydirectinput, time
 import mediapipe as mp
 import numpy as np
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 pyautogui.FAILSAFE = False
+pydirectinput.FAILSAFE = False
 screen_w, screen_h = pyautogui.size()
 last_call = time.perf_counter()
 render_time_total = 0
@@ -29,6 +30,7 @@ eyeball_min_x, eyeball_max_x, eyeball_min_y, eyeball_max_y = -0.0045948644930666
 ALLOW_BOUND_BOX_UPDATES = False
 
 cur_screen_x, cur_screen_y = 0,0
+EXIT_ON_OUT_OF_BOUNDS = False
 
 
 try:
@@ -41,15 +43,13 @@ try:
         frame_h, frame_w, _ = frame.shape
         if landmark_points:
             landmarks = landmark_points[0].landmark
-
-            r_eye_landmarks = np.array(landmarks)[[277, 282, 283, 285, 286, 283, 295, 300, 329, 333, 334, 336, 337, 339, 340, 341, 342, 343, 346, 347, 348, 349]]
             new_r_eye_centroid_x, new_r_eye_centroid_y = 0, 0
             
             # TODO: Try to make this as close to the center of the right eye
             r_eye_indexes = [277, 282, 283, 285, 286, 283, 295, 300, 329, 333, 334, 336, 337, 339, 340, 341, 342, 343, 346, 347, 348, 349]
             r_eye_landmarks = np.array(landmarks)[r_eye_indexes]
             for id, landmark in enumerate(r_eye_landmarks):
-                #cv2.circle(frame, (int(landmark.x * frame_w), int(landmark.y * frame_h)), 3, (0, 0, 255))
+                cv2.circle(frame, (int(landmark.x * frame_w), int(landmark.y * frame_h)), 3, (0, 0, 255))
                 #cv2.putText(frame, str(r_eye_indexes[id]), (int(landmark.x * frame_w), int(landmark.y * frame_h)), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 0), 1)
                 new_r_eye_centroid_x += landmark.x
                 new_r_eye_centroid_y += landmark.y
@@ -126,7 +126,8 @@ try:
                         dist = min(math.sqrt(math.dist((cur_screen_x, cur_screen_y), (screen_x, screen_y))), 100)
                         cur_screen_x, cur_screen_y = cur_screen_x + dist * math.cos(angle_rad), cur_screen_y + dist * math.sin(angle_rad)
                         
-                    pyautogui.moveTo(cur_screen_x, cur_screen_y, _pause=False)
+                    #pyautogui.moveTo(cur_screen_x, cur_screen_y, _pause=False)
+                    pydirectinput.moveTo(int(cur_screen_x), int(cur_screen_y), _pause=False)
                     
             # l_eye = [landmarks[145], landmarks[159]]
             # for landmark in l_eye:
@@ -138,10 +139,11 @@ try:
             # min,max = 300,350
             # for id, landmark in enumerate(landmarks[min:max]):
             #     cv2.putText(frame, str(id+min), (int(landmark.x * frame_w), int(landmark.y * frame_h)), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 0), 1)
-        else:
+        elif EXIT_ON_OUT_OF_BOUNDS:
             print("eyeball_min_x, eyeball_max_x, eyeball_min_y, eyeball_max_y", eyeball_min_x, eyeball_max_x, eyeball_min_y, eyeball_max_y)
             print("end report total and avg:", str(render_time_total).zfill(3), str(render_time_total / total_calls).zfill(3))
             exit(0)
+
         cv2.imshow('Eye Controlled Mouse', frame)
         cv2.waitKey(1)
 except Exception as e:
