8f62b5154b7b8d080db87019126ddfd9bdb9aa72
elliotthuryk <elliott.huryk@gmail.com>
2023-04-29 16:24:59 -0400

    Mouse movement added + smoothed cursor input

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import cv2, pyautogui, time
+import cv2, math, pyautogui, time
 import mediapipe as mp
 import numpy as np
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+pyautogui.FAILSAFE = False
 screen_w, screen_h = pyautogui.size()
 last_call = time.perf_counter()
 render_time_total = 0
@@ -20,7 +21,14 @@ def get_time(tag="", do_print = False):
 
 cur_r_eye_centroid_x, cur_r_eye_centroid_y = 0, 0
 
-eyeball_min_x, eyeball_max_x, eyeball_min_y, eyeball_max_y = -0.02029406753453339, 0.010147208517247985, -0.008228669112378906, 0.007702179930426878
+#Example sets: 
+# Calibration = 0,0,0,0 
+# -0.020294067534533390, 0.010147208517247985, -0.008228669112378906, 0.0077021799304268780
+# -0.004594864493066619, 0.004176107319918521, -0.003290743990377931, 0.0074860494245182485
+eyeball_min_x, eyeball_max_x, eyeball_min_y, eyeball_max_y = -0.004594864493066619, 0.004176107319918521, -0.003290743990377931, 0.0074860494245182485
+ALLOW_BOUND_BOX_UPDATES = False
+
+cur_screen_x, cur_screen_y = 0,0
 
 
 try:
@@ -55,33 +63,70 @@ try:
             cur_r_eye_centroid_x, cur_r_eye_centroid_y = new_r_eye_centroid_x, new_r_eye_centroid_y
 
             # Right Eyeball, create bounding box
+            r_eyeball_x_to_c, r_eyeball_y_to_c = 0, 0
             for id, landmark in enumerate(landmarks[473:474]):
                 cv2.circle(frame, (int(landmark.x * frame_w), int(landmark.y * frame_h)), 3, (0, 255, 0))
 
-                r_eyeball_x = landmark.x - cur_r_eye_centroid_x
-                r_eyeball_y = landmark.y - cur_r_eye_centroid_y
-                #print("".join(["\r", str(r_eyeball_x), " ", str(r_eyeball_y)]))
+                r_eyeball_x_to_c, r_eyeball_y_to_c = landmark.x - cur_r_eye_centroid_x, landmark.y - cur_r_eye_centroid_y
+                #print("".join(["\r", str(r_eyeball_x_to_c), " ", str(r_eyeball_y_to_c)]))
 
-                # Create Bounding Box
-                if r_eyeball_x < eyeball_min_x:
-                    eyeball_min_x = r_eyeball_x
-                if r_eyeball_x > eyeball_max_x:
-                    eyeball_max_x = r_eyeball_x
-                if r_eyeball_y < eyeball_min_y:
-                    eyeball_min_y = r_eyeball_y
-                if r_eyeball_y > eyeball_max_y:
-                    eyeball_max_y = r_eyeball_y
+                if ALLOW_BOUND_BOX_UPDATES:
+                    # Create Bounding Box
+                    if r_eyeball_x_to_c < eyeball_min_x:
+                        eyeball_min_x = r_eyeball_x_to_c
+                    if r_eyeball_x_to_c > eyeball_max_x:
+                        eyeball_max_x = r_eyeball_x_to_c
+                    if r_eyeball_y_to_c < eyeball_min_y:
+                        eyeball_min_y = r_eyeball_y_to_c
+                    if r_eyeball_y_to_c > eyeball_max_y:
+                        eyeball_max_y = r_eyeball_y_to_c
                  
             # Show Bounding Box   
             r_eye_box_start = (int((cur_r_eye_centroid_x + eyeball_min_x) * frame_w), int((cur_r_eye_centroid_y + eyeball_min_y) * frame_h))
             r_eye_box_end = (int((cur_r_eye_centroid_x + eyeball_max_x) * frame_w), int((cur_r_eye_centroid_y + eyeball_max_y) * frame_h))
-            print("r_eye_box_start, r_eye_box_end", r_eye_box_start, r_eye_box_end)
-            cv2.rectangle(frame, r_eye_box_start, r_eye_box_end, (0,255,0), 1)
-            
-            # Move Mouse
-            # screen_x = screen_w * landmark.x
-            # screen_y = screen_h * landmark.y
-            # pyautogui.moveTo(screen_x, screen_y, _pause=False)
+            #print("cur_r_eye_centroid_x, cur_r_eye_centroid_y", cur_r_eye_centroid_x, cur_r_eye_centroid_y)
+            #print("r_eyeball_x_to_c, r_eyeball_y_to_c", r_eyeball_x_to_c, r_eyeball_y_to_c)
+            #print("r_eye_box_start, r_eye_box_end", r_eye_box_start, r_eye_box_end)
+            cv2.rectangle(frame, r_eye_box_start, r_eye_box_end, (0,100,0), 1)
+
+            # Lower Left of screen
+            screen_x = 0
+            screen_y = 0
+
+            if eyeball_min_x != 0 and eyeball_max_x != 0 and eyeball_min_y != 0 and eyeball_max_y != 0:
+                if r_eyeball_x_to_c > 0 and r_eyeball_y_to_c > 0:
+                    per_x = r_eyeball_x_to_c / eyeball_max_x
+                    per_y = r_eyeball_y_to_c / eyeball_max_y
+                    screen_x = (screen_w / 2) + (per_x * screen_w)
+                    screen_y = (screen_h / 2) + (per_y * screen_h)
+
+                elif  r_eyeball_x_to_c < 0 and r_eyeball_y_to_c > 0:
+                    per_x = r_eyeball_x_to_c / eyeball_min_x
+                    per_y = r_eyeball_y_to_c / eyeball_max_y
+                    screen_x = (screen_w / 2) - (per_x * screen_w)
+                    screen_y = (screen_h / 2) + (per_y * screen_h)
+
+                elif r_eyeball_x_to_c > 0 and r_eyeball_y_to_c < 0:
+                    per_x = r_eyeball_x_to_c / eyeball_max_x
+                    per_y = r_eyeball_y_to_c / eyeball_min_y
+                    screen_x = (screen_w / 2) + (per_x * screen_w)
+                    screen_y = (screen_h / 2) - (per_y * screen_h)
+
+                elif  r_eyeball_x_to_c < 0 and r_eyeball_y_to_c < 0:
+                    per_x = r_eyeball_x_to_c / eyeball_min_x
+                    per_y = r_eyeball_y_to_c / eyeball_min_y
+                    screen_x = (screen_w / 2) - (per_x * screen_w)
+                    screen_y = (screen_h / 2) - (per_y * screen_h)
+
+                if screen_x != 0 and screen_y != 0:
+                    if cur_screen_x == 0 and cur_screen_y == 0:
+                        cur_screen_x, cur_screen_y = screen_x, screen_y
+                    else:
+                        angle_rad = math.atan2(screen_y-cur_screen_y, screen_x-cur_screen_x)
+                        dist = min(math.sqrt(math.dist((cur_screen_x, cur_screen_y), (screen_x, screen_y))), 100)
+                        cur_screen_x, cur_screen_y = cur_screen_x + dist * math.cos(angle_rad), cur_screen_y + dist * math.sin(angle_rad)
+                        
+                    pyautogui.moveTo(cur_screen_x, cur_screen_y, _pause=False)
                     
             # l_eye = [landmarks[145], landmarks[159]]
             # for landmark in l_eye:
